62098f16de609730ce3cb7c7f28d408e4caf5b6f
Oleksandr <rigby8806@gmail.com>
2025-01-25 11:25:58 +0200

    Solution

--- movieapi/settings.py
+++ movieapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)vx!iglcoi@ym%_af48nyg1(bz!5c8ok!8^2gz5#kzms0kxe9!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
